c3648bf5934f2ff59348e080f127913ce2a8e256
HAdamCode <hunter.j.adam@gmail.com>
2023-02-05 19:46:57 -0700

    Data and pizza classes setup

---
+++ hadam_A1/app/src/main/java/com/csci448/hadam/hadam_a1/data/Pizza.kt
@@ -0,0 +1,3 @@
+package com.csci448.hadam.hadam_a1.data
+
+data class Pizza(var name: String, var costPerPie: Double)
---
+++ hadam_A1/app/src/main/java/com/csci448/hadam/hadam_a1/data/PizzaRepo.kt
@@ -0,0 +1,13 @@
+package com.csci448.hadam.hadam_a1.data
+
+object PizzaRepo {
+    var pizzas =
+        listOf(
+            Pizza("Pepperoni", 2.12),
+            Pizza("Vegetarian", 3.42),
+            Pizza("Vegan", 5.01),
+            Pizza("Meat Lovers", 2.13),
+            Pizza("Cheese", 2.06),
+            Pizza("Ham and Pineapple", 10.97)
+        )
+}
