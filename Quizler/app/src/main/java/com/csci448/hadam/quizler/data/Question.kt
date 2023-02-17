4af9d051d140dca6ea2f8005902afd455ae4ff32
HAdamCode <hunter.j.adam@gmail.com>
2023-02-16 22:11:09 -0700

    Lab4 started

--- Quizler/app/src/main/java/com/csci448/hadam/quizler/data/Question.kt
+++ Quizler/app/src/main/java/com/csci448/hadam/quizler/data/Question.kt
@@ -1,5 +1,9 @@
 package com.csci448.hadam.quizler.data
 
 import androidx.annotation.StringRes
+import androidx.compose.ui.res.stringResource
+import com.csci448.hadam.quizler.R
 
-data class Question(@StringRes val questionTextId: Int, val isTrue: Boolean)
+data class Question(@StringRes val questionTextId: Int, val correctChoiceNumber: Int, val choice1Id: String = stringResource(
+    id = R.string.label_true
+), val choice2Id: String)
