2f3c2cdcb8c358ba1aee7d6fce8910221075dc92
HAdamCode <hunter.j.adam@gmail.com>
2023-04-04 11:47:12 -0600

    Lab 10b finished up

--- GeoLocatr/app/src/main/java/com/csci448/hadam/geolocatr/LocationScreen.kt
+++ GeoLocatr/app/src/main/java/com/csci448/hadam/geolocatr/LocationScreen.kt
@@ -2,15 +2,46 @@ package com.csci448.hadam.geolocatr
 
 import android.location.Location
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun LocationScreen(location: Location?, locationAvailable: Boolean, onGetLocation: () -> Unit, address: String ) {
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(0.0,0.0), 0f)
+    }
+    val context = LocalContext.current
+    LaunchedEffect(location) {
+        if(location != null) {
+            // include all points that should be within the bounds of the zoom
+            // convex hull
+            val bounds = LatLngBounds.Builder()
+                .include(LatLng(location.latitude, location.longitude)).build()
+                    // add padding
+                    val padding = context.resources
+                    .getDimensionPixelSize(R.dimen.map_inset_padding)
+            // create a camera update to smoothly move the map view
+            val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+            // move our camera!
+            cameraPositionState.animate(cameraUpdate)
+        }
+    }
     Column() {
         Text(text = "Latitude / Longitude")
         if (location != null) {
@@ -20,6 +51,21 @@ fun LocationScreen(location: Location?, locationAvailable: Boolean, onGetLocatio
         Text(text = address)
         Button(onClick = onGetLocation, enabled = locationAvailable) {
             Text("Get Current Location")
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            if(location != null) {
+                val markerState = MarkerState().apply {
+                    position = LatLng(location.latitude, location.longitude)
+                }
+                Marker(
+                    state = markerState,
+                    title = address,
+                    snippet = "${location.latitude} / ${location.longitude}"
+                )
+            }
         }
 
     }
