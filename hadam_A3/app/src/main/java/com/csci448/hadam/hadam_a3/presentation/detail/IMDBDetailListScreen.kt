965d8c9604cc2bb558235d8f7bea65095af88a04
hunteradam <hadam@mines.edu>
2023-04-18 09:47:49 -0600

    CHange Card to play button

--- hadam_A3/app/src/main/java/com/csci448/hadam/hadam_a3/presentation/detail/IMDBDetailListScreen.kt
+++ hadam_A3/app/src/main/java/com/csci448/hadam/hadam_a3/presentation/detail/IMDBDetailListScreen.kt
@@ -1,18 +1,26 @@
 package com.csci448.hadam.hadam_a3.presentation.detail
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.csci448.hadam.hadam_a3.R
 import com.csci448.hadam.hadam_a3.data.titlevideo.Video
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,27 +29,35 @@ fun IMDBDetailListScreen(video: Video, onVideoClick: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(7.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-        onClick = {onVideoClick()}
+//        onClick = {onVideoClick()}
     ) {
         Row(modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()) {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth(.8f)
+                    .fillMaxWidth(.7f)
             ) {
                 Text(text = video.title, fontWeight = FontWeight.Bold)
                 video.description?.let { Text(text = it) }
             }
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxWidth(.5f)
             ) {
                 val minutes = video.durationInSeconds % 60
                 val hours = video.durationInSeconds / 60
                 Text(text = "${hours}:${minutes}")
+            }
+            Column() {
+                IconButton(onClick = { onVideoClick() }) {
+                    Icon(
+                        imageVector = Icons.Filled.PlayArrow,
+                        contentDescription = stringResource(R.string.list_name)
+                    )
+                }
             }
         }
     }
