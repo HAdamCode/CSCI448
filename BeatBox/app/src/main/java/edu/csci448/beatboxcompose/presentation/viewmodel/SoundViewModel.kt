0af4bc74d7b1fdd541e784786b6e432cf098087d
HAdamCode <hunter.j.adam@gmail.com>
2023-04-21 21:38:23 -0600

    Beatbox initial code and lab/slides documentation added

---
+++ BeatBox/app/src/main/java/edu/csci448/beatboxcompose/presentation/viewmodel/SoundViewModel.kt
@@ -0,0 +1,18 @@
+package edu.csci448.beatboxcompose.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import edu.csci448.beatboxcompose.util.BeatBox
+import edu.csci448.beatboxcompose.data.Sound
+
+class SoundViewModel(private val beatBox: BeatBox) : ViewModel() {
+    val sounds = beatBox.sounds
+
+    fun onPlaySound(sound: Sound?) {
+        // TODO have the BeatBox play the sound!
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        beatBox.release()
+    }
+}
